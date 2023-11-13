3c660863f603d2cc3492d6bbc867484ba5b2799a
Mateo-Alvarez1 <mateoalvarez384@gmail.com>
2023-11-13 18:58:25 -0300

    imagenes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,9 @@ export const About = () => {
       <h3>Backend Developer</h3>
         <p>As a developer, I highlight my solid experience in <span>building infrastructures and implementing logics</span> that support the optimal functioning of systems. My focus on detail and ability to translate ideas into code allow me to create efficient <span>solutions</span>. I have demonstrated these skills in several projects, where I have worked with cutting-edge technologies such as <span>Java, SpringBoot, MySQL and Docker</span>. My experience is not only limited to the back-end, as I also have skills in full-stack development, excelling in <span>React</span>.</p>
       </div>
+      <div className='button'>
         <a href='#contact'><button>Contact</button></a>
+      </div>
     </div>
     </>
   )
